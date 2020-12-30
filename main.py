af4ed26c584b9f05f1ddf3759033cee02c1d4a9c
SamroodAli <samroodali@live.com>
2020-12-30 18:34:08 +0530

    Add:for loop writing to a file

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 """Main project file"""
-with open("../Input/Names/invited_names.txt") as names:
+with open("Input/Names/invited_names.txt") as names:
     names_list = names.readlines()
 
-with open("../Input/Letters/starting_letter.docx") as starting_letter:
+with open("Input/Letters/starting_letter.docx") as starting_letter:
     content = starting_letter.read()
+
+
+for name in names_list:
+    name = name.replace("\n", "")
+    name = name.strip()
+    with open(f"Output/ReadyToSend/letter_for_{name}.docx", mode="w") as letter:
+        letter_content = content.replace("[name]", name)
+        letter.write(letter_content)
