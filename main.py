edfdc1017a433b10ab1b5fa07411973657bae5b5
SamroodAli <samroodali@live.com>
2020-12-30 18:52:55 +0530

    Add:replace method for names

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ with open("Input/Names/invited_names.txt") as names:
 with open("Input/Letters/starting_letter.docx") as starting_letter:
     sample_content = starting_letter.read()
     for name in names_list:
+        name = name.replace("\n", "")
         stripped_name = name.strip()
         with open(f"Output/ReadyToSend/letter_for_{name}.docx", mode="w") as letter:
             letter_content = sample_content.replace(PLACEHOLDER, stripped_name)
