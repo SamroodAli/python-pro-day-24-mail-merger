6a279e0288d4a8d10dfe402cd3dff33c49405e76
SamroodAli <samroodali@live.com>
2020-12-30 18:44:17 +0530

    Refactor:placeholder variable, for loop inside with statement

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 """Main project file"""
+
+PLACEHOLDER = "[name]"
 with open("Input/Names/invited_names.txt") as names:
     names_list = names.readlines()
 
 with open("Input/Letters/starting_letter.docx") as starting_letter:
-    content = starting_letter.read()
-
-
-for name in names_list:
-    name = name.replace("\n", "")
-    name = name.strip()
-    with open(f"Output/ReadyToSend/letter_for_{name}.docx", mode="w") as letter:
-        letter_content = content.replace("[name]", name)
-        letter.write(letter_content)
+    sample_content = starting_letter.read()
+    for name in names_list:
+        stripped_name = name.strip()
+        with open(f"Output/ReadyToSend/letter_for_{name}.docx", mode="w") as letter:
+            letter_content = sample_content.replace(PLACEHOLDER, stripped_name)
+            letter.write(letter_content)
