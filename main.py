31a2ae23e344c1993c5cfac72dfbbd70b9296884
SamroodAli <samroodali@live.com>
2020-12-30 18:22:53 +0530

    Add:sample letter to variable content

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 """Main project file"""
 with open("../Input/Names/invited_names.txt") as names:
     names_list = names.readlines()
+
+with open("../Input/Letters/starting_letter.docx") as starting_letter:
+    content = starting_letter.read()
